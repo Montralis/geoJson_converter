ad7645da06c3d542ad99c4212093e9a97072ea92
Leon <Leonth123@gmail.com>
2023-09-09 13:34:28 +0200

    fix(overlapping points): enlarge possible polygons

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import json
-import sys
-import math
+import string
 from shapely.geometry import Point, Polygon
 from shapely.ops import transform
 import pyproj
 from pyproj.crs import ProjectedCRS
 from pyproj.crs.coordinate_operation import AzimuthalEquidistantConversion
 from geopy.distance import geodesic
+import numpy as np
 
+# return the closest that is found in a given array of points
 def find_closest_point(origin, points):
     min_distance = float('inf')
     closest_point = None
@@ -18,7 +19,7 @@ def find_closest_point(origin, points):
             closest_point = point
     return closest_point
 
-# Function to calculate the geodesic point buffer taking into account the projection
+# Calculate the geodesic point buffer from config points in EPSG:4236
 def geodesic_point_buffer(lon, lat, radius_m):
     proj_crs = ProjectedCRS(
         conversion=AzimuthalEquidistantConversion(lat, lon)
@@ -31,11 +32,11 @@ def geodesic_point_buffer(lon, lat, radius_m):
     ).transform
 
     return transform(Trans, Point(0, 0).buffer(radius_m))
-    
 
 def calculate_midpoint(point1, point2):
     return [(point1[0] + point2[0]) / 2, (point1[1] + point2[1]) / 2]
 
+# removes all points that are inside another given polygon 
 def remove_overlapping_points(original_polygon, overlapping_polygon):
         # Transform the circle into a GeoJSON-compatible polygon
     overlapping_polygon_geojson = {
@@ -53,6 +54,23 @@ def remove_overlapping_points(original_polygon, overlapping_polygon):
 
     return overlapping_polygon_filtered
 
+# creates a rectangle-polygon given from two points
+# direction can only be -1 for counterclockwise or 1 for clockwise
+def create_rectangle_from_points(point1, point2, direction=-1):
+    # Calculate the vector between the points
+    vector = np.array([point2[0] - point1[0], point2[1] - point1[1]])
+
+    # Create a vector that is 90 degrees counterclockwise to the normalized vector
+    perpendicular_vector = np.array([-vector[1], vector[0]]) if direction == -1 else (np.array([vector[1], -vector[0]]) if direction == 1 else None)
+
+    # Move point1 and point2 downward along the perpendicular vector to create a rectangle
+    new_point1 = Point(point1[0] + perpendicular_vector[0], point1[1] + perpendicular_vector[1])
+    new_point2 = Point(point2[0] + perpendicular_vector[0], point2[1] + perpendicular_vector[1])
+
+    # Define the rectangle by connecting the new points
+    return [new_point1, new_point2, point2, point1]
+
+
 def main():
     # Load the original GeoJSON file
     with open('data.geojson', 'r') as f:
@@ -63,7 +81,7 @@ def main():
         config_data = json.load(f)
     
     for config in config_data:
-        polygon_number = config['polygonnumber']
+        polygon_number = config['polygonNumber']
         insert_between = config['insertBetween']
 
         polygon_coordinates = geojson_data['features'][polygon_number - 1]['geometry']['coordinates'][0]
@@ -78,14 +96,14 @@ def main():
 
             # Calculate the midpoint between the input points
             midpoint = calculate_midpoint(point_1, point_2)
-
-            distance_meters = geodesic((point_1[1], point_1[0]), (point_2[1], point_2[0])).meters
-            radius = distance_meters / 2
+            radius = geodesic((point_1[1], point_1[0]), (point_2[1], point_2[0])).meters / 2 
 
             # Generate the circle as a Shapely polygon considering the projection
             circle = geodesic_point_buffer(midpoint[0], midpoint[1], radius)
 
-            filtered_circle_points = remove_overlapping_points(polygon_coordinates, circle)
+            # Remove unused points from circle
+            tmp_rec_poly = create_rectangle_from_points(point_1, point_2)
+            filtered_circle_points = remove_overlapping_points(tmp_rec_poly, circle)
             inserted_points = []
             current_point = (point_1[0], point_1[1])
 
